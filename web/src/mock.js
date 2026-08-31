962e53b9d58643261537c111684912dce247de8d
lehhair <lehhair@users.noreply.github.com>
2026-08-31 10:26:14 +0800

    release: v0.3.4

--- web/src/mock.js
+++ web/src/mock.js
@@ -61,7 +61,7 @@ export const mockBridge = {
   appInfo: () =>
     j({
       desktop: !previewMobile,
-      version: '0.3.3',
+      version: '0.3.4',
       bundled: false,
       platform: previewMobile ? 'android' : 'windows',
     }),
